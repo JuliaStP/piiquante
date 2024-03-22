daf987219038acdb5a860a9a7bfd8e670dc9d828
JuliaStP <andreeva_julia@hotmail.com>
2024-03-22 16:39:55 -0400

    cleaned up app.js file

--- back/app.js
+++ back/app.js
@@ -1,6 +1,3 @@
-//mongodb link: mongodb+srv://<username>:<password>@clusterpiiquante.s8tanyd.mongodb.net/?retryWrites=true&w=majority&appName=clusterPiiquante
-//mongodb password: 10Marina
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,7 +5,6 @@ const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
-// const Thing = require('./models/thing');
 const app = express();
 app.use(bodyParser.json()); 
 
@@ -34,63 +30,5 @@ mongoose.connect('mongodb+srv://andreevajulia:10Marina@clusterpiiquante.s8tanyd.
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api', stuffRoutes);
 app.use('/api/auth', userRoutes);
-
-
-//   app.get('/api/stuff', (req, res, next) => {
-//     Thing.find().then(
-//       (things) => {
-//         res.status(200).json(things);
-//       }
-//     ).catch(
-//       (error) => {
-//         res.status(400).json({
-//           error: error
-//         });
-//       }
-//     );
-//   });
-
-
-//   app.post('/api/stuff', (req, res, next) => {
-//     const thing = new Thing({
-//       title: req.body.title,
-//       description: req.body.description,
-//       imageUrl: req.body.imageUrl,
-//       price: req.body.price,
-//       userId: req.body.userId
-//     });
-//     thing.save().then(
-//       () => {
-//         res.status(201).json({
-//           message: 'Post saved successfully!'
-//         });
-//       }
-//     ).catch(
-//       (error) => {
-//         res.status(400).json({
-//           error: error
-//         });
-//       }
-//     );
-//   });
-
-// app.use((req, res, next) => {
-//   console.log('Request received!');
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   res.status(201);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   res.json({ message: 'Your request was successful!' });
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log('Response sent successfully!');
-// });
 
 module.exports = app;
